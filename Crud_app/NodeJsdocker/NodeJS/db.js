f31e32b554bfb515e462e4e41a3100b8421b5d6d
sam-999 <lebekouche.sa@gmail.com>
2020-11-13 17:05:58 +0100

    docker_compose_1.0

---
+++ Crud_app/NodeJsdocker/NodeJS/db.js
@@ -0,0 +1,11 @@
+const  mongoose = require('mongoose');
+
+mongoose.connect('mongodb://mongodb1:27017/CrudDB',{ useNewUrlParser: true , useUnifiedTopology: true,useFindAndModify: false },(err)=>{
+  if(!err)
+    console.log('MongoDB connection succeeded....');
+  else
+    console.log('Error in DB connection:' + JSON.stringify(err, undefined,2));
+
+});
+
+module.exports = mongoose;
